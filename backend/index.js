b1d311a7afa255833161913e4fcd6631c820d9d0
it62070180 <56427172+it62070180@users.noreply.github.com>
2022-05-10 12:30:25 +0700

    Prepare

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,33 @@
-{
-    const express = require("express")
-    const cors = require('cors')
-    const path = require("path")
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
+const app = express()
+const port = process.env.PORT || 3001
+const { MongoClient } = require("mongodb");
+const uri = process.env.DB;
 
-    const app = express();
+app.use(cors())
+app.use(express.json());
 
-    const PORT = process.env.PORT || 3001;
+app.get('/', (req, res) => {
+  res.send("Server running la")
+})
 
-    app.get("/", (req, res) => {
-        res.json({ message: "Hello from server!" });
-      });
+app.get('/test', async(req, res) => {
+  const client = new MongoClient(uri);
+  await client.connect();
+  const users = await client.db('Final_exam_SDTE').collection('test').find({}).toArray();
+  await client.close();
+  if(users){
+    console.log(users);
+    res.status(200).send(users);
+  }else{
+    console.log("Error found.")
+    res.status(404).send("Error can't do ops.");
+  }
+  
+})
 
-    app.listen(PORT, () => {
-        console.log(`Server listening on ${PORT}`);
-      });
-}
+app.listen(port, () => {
+  console.log(`Server run at http://localhost:${port}`)
+})
